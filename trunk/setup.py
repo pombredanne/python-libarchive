e4e608c121729c6f0576532e6ff66f260c0b1f24
btimby <btimby@4da9454a-e7c2-6ce6-ca87-bb5f0fc19e39>
2012-03-07 18:29:03 +0000

    Readying for new release

--- trunk/setup.py
+++ trunk/setup.py
@@ -37,11 +37,38 @@ except ImportError:
 
 name = 'python-libarchive'
 version = '3.0.3'
-release = '4'
+release = '7'
 versrel = version + '-' + release
 download_url = "http://" + name + ".googlecode.com/files/" + name + "-" + \
                                                           versrel + ".tar.gz"
+description = """\
+A `SmartFile`_ Open Source project. `Read more`_ about how SmartFile
+uses and contributes to Open Source software.
 
+.. figure:: http://www.smartfile.com/images/logo.jpg
+   :alt: SmartFile
+
+Introduction
+------------
+A complete wrapper for the libarchive library generated using SWIG.
+Also included in the package are compatibility layers for the Python
+zipfile and tarfile modules.
+
+Libarchive supports the following:
+
+ - Reads a variety of formats, including tar, pax, cpio, zip, xar, lha, ar, cab, mtree, rar, and ISO images.
+ - Writes tar, pax, cpio, zip, xar, ar, ISO, mtree, and shar archives.
+ - Automatically handles archives compressed with gzip, bzip2, lzip, xz, lzma, or compress.
+
+For information on installing libarchive and python-libarchive, see the `Building`_.
+
+For information on using python-libarchive, see `Examples`_
+
+.. _SmartFile: http://www.smartfile.com/
+.. _Read more: http://www.smartfile.com/open-source.html
+.. _Building: http://code.google.com/p/python-libarchive/wiki/Building
+.. _Examples: http://code.google.com/p/python-libarchive/wiki/Examples
+"""
 
 class build_ext_extra(build_ext, object):
     """
@@ -94,10 +121,7 @@ __libarchive = Extension(name='libarchive.__libarchive',
 setup(name = name,
       version = versrel,
       description = 'A libarchive wrapper for Python.',
-      long_description = '''\
-A complete wrapper for the libarchive library generated using SWIG.
-Also included in the package are compatibility layers for the Python
-zipfile and tarfile modules.''',
+      long_description = description,
       license = 'BSD-style license',
       platforms = ['any'],
       author = 'Ben Timby',
